a7f04a646531901d476105b673ddff60650b11d0
Saqib Ali <docbook.xml@gmail.com>
2022-11-27 11:00:47 -0800

    adding humanize_time_span.sql

--- humanize_time_span.sql
+++ humanize_time_span.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE FUNCTION humanize_time_span(post_date timestamp) RETURNS varchar AS
+CREATE OR REPLACE FUNCTION humanize_time_span(ts timestamp) RETURNS varchar AS
 '
 SELECT
   CASE
-    WHEN (now() - post_date::timestamp)::INT < 60 THEN '' Just Posted''
-    WHEN ((now() - post_date::timestamp)::INT) / 60 < 60 THEN ((now() - post_date::TIMESTAMPTZ)::INT / 60)::INT || '' Minutes ago''
-    WHEN (now() - post_date::TIMESTAMPTZ)::INT / (60 * 60) < 24 THEN ((now() - post_date::TIMESTAMPTZ)::INT / (60 * 60))::INT || '' Hours ago''
-    ELSE ((now() - post_date::TIMESTAMPTZ)::INT / (60 * 60 * 24))::INT || '' Days ago''
+    WHEN (now() - ts::timestamp)::INT < 60 THEN '' Just Posted''
+    WHEN ((now() - ts::timestamp)::INT) / 60 < 60 THEN ((now() - ts::TIMESTAMPTZ)::INT / 60)::INT || '' Minutes ago''
+    WHEN (now() - ts::TIMESTAMPTZ)::INT / (60 * 60) < 24 THEN ((now() - ts::TIMESTAMPTZ)::INT / (60 * 60))::INT || '' Hours ago''
+    ELSE ((now() - ts::TIMESTAMPTZ)::INT / (60 * 60 * 24))::INT || '' Days ago''
   END
 '
 LANGUAGE SQL;
